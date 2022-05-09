ac88f6fd36a78dd72be14b7a9d7031b83a94396a
Joseph Brooksbank <joseph.brooksbank@navexglobal.com>
2022-05-09 10:25:46 -0700

    Add pyscript

---
+++ python_script/rename_files.py
@@ -0,0 +1,24 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import re
+
+DIRNAME = '../scripts'
+
+file_name_valid = re.compile('^\d{5}.*\.sql')
+
+only_files = [f for f in listdir(DIRNAME) if isfile(join(DIRNAME, f))]
+
+success_files = [f for f in only_files if file_name_valid.match(f)]
+fail_files = [f for f in only_files if not file_name_valid.match(f)]
+
+success_numbers = [int(f[0:5]) for f in success_files]
+next_number = max(success_numbers) + 1
+
+for f in fail_files:
+    number_string = f"{next_number:05d}c-"
+    os.rename(join(DIRNAME, f), join(DIRNAME, number_string+f))
+print(only_files)
+print(success_files)
+print(fail_files)
+print(f"{max(success_numbers):05d}")
