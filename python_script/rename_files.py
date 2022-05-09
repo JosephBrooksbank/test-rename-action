f908016cbafbb1c202f17f354768a6305a7de15e
Joseph Brooksbank <joseph.brooksbank@navexglobal.com>
2022-05-09 10:33:29 -0700

    change

--- python_script/rename_files.py
+++ python_script/rename_files.py
@@ -13,6 +13,10 @@ success_files = [f for f in only_files if file_name_valid.match(f)]
 fail_files = [f for f in only_files if not file_name_valid.match(f)]
 
 success_numbers = [int(f[0:5]) for f in success_files]
+
+print(only_files)
+print(success_files)
+print(fail_files)
 next_number = max(success_numbers) + 1
 
 for f in fail_files:
